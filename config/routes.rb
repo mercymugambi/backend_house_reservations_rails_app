c11db3222235364e971c8b7fbc85227e703369f9
Joy Phoebe <joyapisi@gmail.com>
2023-08-17 16:09:52 +0300

    Add routes for houses and reservations

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :reservations
-  resources :houses
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'root#index'
+ 
+  resources :houses, only: %i[index show new create destroy] do
+    resources :reservations, only: %i[index show new create destroy]
+  end
 
   namespace :api do
     namespace :v1 do
