423b23228c77956415ce5746ec9c31bb9f59da56
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-08-18 15:51:10 +0500

    uptaded columns

--- db/migrate/20230817130006_create_reservations.rb
+++ db/migrate/20230817130006_create_reservations.rb
@@ -2,7 +2,8 @@ class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
       t.string :city
-      t.date :reservation_date
+      t.date :reservation_checkin_date
+      t.date :reservation_checkout_date
       t.references :user, null: false, foreign_key: true
       t.references :house, null: false, foreign_key: true
 
