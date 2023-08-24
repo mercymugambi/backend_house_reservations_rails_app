bc3e4eb088be63456bea137120883c58d1236e56
Joy Phoebe <joyapisi@gmail.com>
2023-08-24 21:53:33 +0300

    Change column name to house_name

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::HousesController < ApplicationController
-  load_and_authorize_resource
+  # load_and_authorize_resource
   before_action :set_house, only: %i[show edit update destroy]
 
   # GET /houses or /houses.json
@@ -22,15 +22,21 @@ class Api::V1::HousesController < ApplicationController
   # POST /houses or /houses.json
   def create
     @house = House.new(house_params)
+    # @house = House.new(params[:house]) 
 
-    respond_to do |format|
-      if @house.save
-        format.html { redirect_to house_url(@house), notice: 'House was successfully created.' }
-        format.json { render :show, status: :created, location: @house }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @house.errors, status: :unprocessable_entity }
-      end
+    # respond_to do |format|
+    #   if @house.save
+    #     format.html { redirect_to house_url(@house), notice: 'House was successfully created.' }
+    #     format.json { render :show, status: :created, location: @house }
+    #   else
+    #     format.html { render :new, status: :unprocessable_entity }
+    #     format.json { render json: @house.errors, status: :unprocessable_entity }
+    #   end
+    # end
+    if @house.save
+      render json: { status: "success", message: "House created successfully!" }, status: :created
+    else
+      render json: { status: "failed", errors: @house.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
@@ -66,6 +72,6 @@ class Api::V1::HousesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def house_params
-    params.require(:house).permit(:icon, :name, :description, :user_id)
+    params.require(:house).permit(:icon, :name, :city, :description, :bedrooms, :bathrooms, :rent, :security_deposit, :contact_phone_number, :admin_user_id)
   end
 end
