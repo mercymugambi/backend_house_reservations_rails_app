7a0ed51cf07b2f57c058159e9eaa3ddf146c5043
Joy Phoebe <joyapisi@gmail.com>
2023-08-28 21:55:53 +0300

    Add route for getting city column in houses table

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
       devise_for :users
       resources :users, only: [:index]
       resources :houses, only: %i[index create new show destroy] do
+        collection do
+          get 'unique_cities', to: 'houses#unique_cities'
+        end  
         resources :reservations, only: %i[index create new show destroy]
       end
     end
